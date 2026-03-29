80d9bc8ac60606213075e3237072a5871e97fdbd
danielstevee <nifangzuo@gmail.com>
2026-03-29 14:23:17 +0700

    rapiin code lagi

--- Smoothing & Sharpening/Sharpening.py
+++ Smoothing & Sharpening/Sharpening.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 
 image = cv2.imread("gambar.jpg")
-
-
 kernel = np.array([[0, -1, 0],
                    [-1, 5, -1],
                    [0, -1, 0]])
@@ -12,6 +10,5 @@ sharpened = cv2.filter2D(image, -1, kernel)
 
 cv2.imshow("Original", image)
 cv2.imshow("Sharpened (Laplacian)", sharpened)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
