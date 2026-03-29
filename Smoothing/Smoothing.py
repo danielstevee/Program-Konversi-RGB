a69ef3884f49eec4a51f27f512da709bff5c2302
danielstevee <nifangzuo@gmail.com>
2026-03-29 10:32:29 +0700

    Update : Penambahan Smoothing citra berwarna

---
+++ Smoothing/Smoothing.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("gambar.jpg")
+
+smoothed = cv2.blur(image, (5, 5))
+
+cv2.imshow("Original", image)
+cv2.imshow("Smoothed (Mean Filter)", smoothed)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
